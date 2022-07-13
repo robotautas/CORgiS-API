310d114f07c3a44c317d558e9f057ac9968d1d22
Artur <107410100+Agent-0013@users.noreply.github.com>
2022-07-13 16:00:51 +0300

    Papildem 3 naujom funkcijom ir prapletem parametru
    T00...T21, P00...P19, 
    GET_PRES; GET_TEMP; GET_DATA;

--- python_files/sketch.c
+++ python_files/sketch.c
@@ -1,3 +1,4 @@
+
 #include <Arduino_HTS221.h>
 // read all the sensor values
 //float temperature = HTS.readTemperature();
@@ -9,7 +10,7 @@
 
 
 
-#define FWver 210806
+#define FWver 220713
 //************************************ Timer config************************************
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
@@ -26,7 +27,7 @@ char stringidx = 0;
 
 //************************************ Output config************************************
 char V00, V01, V02, V03, V04, V05, V06, V07, V08 = 0; // Biary valve set
-unsigned int  T00, T01, T02, T03, T04, T05, T06, T07, T08; //Temperature setpoints
+unsigned int  T00, T01, T02, T03, T04, T05, T06, T07, T08, T09, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21; //Temperature setpoints
 unsigned int S00, S01 = 0;
 bool Pump_cmd = false;
 bool Data_cmd = false;
@@ -101,8 +102,12 @@ void loop() {
   //************************************ Decode Serial Command ************************************
   if (stringComplete) {
     strcpy(tempChars, serialString);  // Copy peceived string to temp buffer
-    if (debug) {Serial.print("OK: ");}
-    if (debug) {Serial.println(tempChars);}
+    if (debug) {
+      Serial.print("OK: ");
+    }
+    if (debug) {
+      Serial.println(tempChars);
+    }
     parseData();
 
     // clear the string:
@@ -170,10 +175,17 @@ void parseData() {      // split the data into its parts
         if ((tmpval < 0) || (tmpval > 999))
         {
           tmpval = 0;
-          if (debug) {Serial.println("Range Error");}
+          if (debug) {
+            Serial.println("Range Error");
+          }
         }
         switch (selector)
         {
+          case 0:
+            {
+              T00 = tmpval;
+              break;
+            }
           case 1:
             {
               T01 = tmpval;
@@ -214,9 +226,76 @@ void parseData() {      // split the data into its parts
               T08 = tmpval;
               break;
             }
+          case 9:
+            {
+              T09 = tmpval;
+              break;
+            }
+          case 10:
+            {
+              T10 = tmpval;
+              break;
+            }
+          case 11:
+            {
+              T11 = tmpval;
+              break;
+            }
+          case 12:
+            {
+              T12 = tmpval;
+              break;
+            }
+          case 13:
+            {
+              T13 = tmpval;
+              break;
+            }
+          case 14:
+            {
+              T14 = tmpval;
+              break;
+            }
+          case 15:
+            {
+              T15 = tmpval;
+              break;
+            }
+          case 16:
+            {
+              T16 = tmpval;
+              break;
+            }
+          case 17:
+            {
+              T17 = tmpval;
+              break;
+            }
+          case 18:
+            {
+              T18 = tmpval;
+              break;
+            }
+          case 19:
+            {
+              T19 = tmpval;
+              break;
+            }
+          case 20:
+            {
+              T20 = tmpval;
+              break;
+            }
+          case 21:
+            {
+              T21 = tmpval;
+              break;
+            }
           default:
             {
-              if (debug) {Serial.println("Selecor Error");}
+              if (debug) {
+                Serial.println("Selecor Error");
+              }
               break;
             }
         }   // End of Switch Temp Selector
@@ -231,7 +310,9 @@ void parseData() {      // split the data into its parts
         if ((tmpval < 0) || (tmpval > 255))
         {
           tmpval = 0;
-          if (debug) {Serial.println("Range Error");}
+          if (debug) {
+            Serial.println("Range Error");
+          }
         }
         switch (selector)
         {
@@ -282,7 +363,9 @@ void parseData() {      // split the data into its parts
             }
           default:
             {
-              if (debug) {Serial.println("Selecor Error");}
+              if (debug) {
+                Serial.println("Selecor Error");
+              }
               break;
             }
         }   // End of Switch valve Selector
@@ -290,7 +373,9 @@ void parseData() {      // split the data into its parts
       }
       else
       {
-        if (debug) {Serial.println("Wrong Command!");}
+        if (debug) {
+          Serial.println("Wrong Command!");
+        }
       }
       parsedString[0] = '\0';
       DataString[0] = '\0';
@@ -307,16 +392,30 @@ void parseData() {      // split the data into its parts
       {
         PrintAll();
       }
+      else if (strcmp(DataString, "TEMP") == 0)   // Print All
+      {
+        PrintTemp();
+      }
+      else if (strcmp(DataString, "PRES") == 0)   // Print All
+      {
+        PrintPres();
+      }
+      else if (strcmp(DataString, "DATA") == 0)   // Print All
+      {
+        PrintData();
+      }
       else
       {
-        if (debug) {Serial.println("Wrong Command!");};
+        if (debug) {
+          Serial.println("Wrong Command!");
+        };
       }
       DataString[0] = '\0';
       parsedString[0] = '\0';
 
     }   // End of "GET"
 
-   
+
 
     else if (strcmp(parsedString, "PUMP") == 0)    // Sample time external setting
     {
@@ -343,7 +442,9 @@ void parseData() {      // split the data into its parts
     }   // End of "PUMP"
     else
     {
-      if (debug) {Serial.println("Unknown command!");};
+      if (debug) {
+        Serial.println("Unknown command!");
+      };
       parsedString[0] = '\0';
     };
 
@@ -379,23 +480,54 @@ void PrintAll(void)
   Serial.print(";V08=");
   Serial.print(V08, HEX);
 
-  Serial.print(";T01=");
-  Serial.print(T01+random(0,3), DEC);
-  Serial.print(";T02=");
-  Serial.print(T02+random(0,3), DEC);
-  Serial.print(";T03=");
-  Serial.print(T03+random(0,3), DEC);
-  Serial.print(";T04=");
-  Serial.print(T04+random(0,3), DEC);
-  Serial.print(";T05=");
-  Serial.print(T05+random(0,3), DEC);
-  Serial.print(";T06=");
-  Serial.print(T06+random(0,3), DEC);
-  Serial.print(";T07=");
-  Serial.print(T07+random(0,3), DEC);
-  Serial.print(";T08=");
-  Serial.print(T08+random(0,3), DEC);
 
+  Serial.print(";T00=");
+  Serial.print(T00 + random(0, 3), DEC);
+  Serial.print(";T01=");
+  Serial.print(T01 + random(0, 3), DEC);
+  Serial.print(";T02=");
+  Serial.print(T02 + random(0, 3), DEC);
+  Serial.print(";T03=");
+  Serial.print(T03 + random(0, 3), DEC);
+  Serial.print(";T04=");
+  Serial.print(T04 + random(0, 3), DEC);
+  Serial.print(";T05=");
+  Serial.print(T05 + random(0, 3), DEC);
+  Serial.print(";T06=");
+  Serial.print(T06 + random(0, 3), DEC);
+  Serial.print(";T07=");
+  Serial.print(T07 + random(0, 3), DEC);
+  Serial.print(";T08=");
+  Serial.print(T08 + random(0, 3), DEC);
+  Serial.print(";T09=");
+  Serial.print(T09 + random(0, 3), DEC);
+  Serial.print(";T10=");
+  Serial.print(T10 + random(0, 3), DEC);
+  Serial.print(";T11=");
+  Serial.print(T11 + random(0, 3), DEC);
+  Serial.print(";T12=");
+  Serial.print(T12 + random(0, 3), DEC);
+  Serial.print(";T13=");
+  Serial.print(T13 + random(0, 3), DEC);
+  Serial.print(";T14=");
+  Serial.print(T14 + random(0, 3), DEC);
+  Serial.print(";T15=");
+  Serial.print(T15 + random(0, 3), DEC);
+  Serial.print(";T16=");
+  Serial.print(T16 + random(0, 3), DEC);
+  Serial.print(";T17=");
+  Serial.print(T17 + random(0, 3), DEC);
+  Serial.print(";T18=");
+  Serial.print(T18 + random(0, 3), DEC);
+  Serial.print(";T191=");
+  Serial.print(T19 + random(0, 3), DEC);
+  Serial.print(";T20=");
+  Serial.print(T20 + random(0, 3), DEC);
+  Serial.print(";T21=");
+  Serial.print(T21 + random(0, 3), DEC);
+
+  Serial.print(";P00=");
+  Serial.print(Pressi);
   Serial.print(";P01=");
   Serial.print(Pressi);
   Serial.print(";P02=");
@@ -412,6 +544,8 @@ void PrintAll(void)
   Serial.print(Pressi);
   Serial.print(";P08=");
   Serial.print(Pressi);
+  Serial.print(";P09=");
+  Serial.print(Pressi);
 
   Serial.print(";S00=");
   Serial.print("00");
@@ -419,5 +553,167 @@ void PrintAll(void)
   Serial.print("00");
   Serial.print(";PUMP=");
   Serial.print(Pump_cmd, BIN);
+  Serial.println(";");
+}
+
+
+void PrintTemp(void)
+{
+
+  // Temporary debug
+  HTStemp = 13; //HTS.readTemperature() + 0.5;
+  int HTStempi = HTStemp;
+  int Pressi = 990; //(BARO.readPressure() + 0.5)*10;
+
+  Serial.print("T00=");
+  Serial.print(T00 + random(0, 3), DEC);
+  Serial.print(";T01=");
+  Serial.print(T01 + random(0, 3), DEC);
+  Serial.print(";T02=");
+  Serial.print(T02 + random(0, 3), DEC);
+  Serial.print(";T03=");
+  Serial.print(T03 + random(0, 3), DEC);
+  Serial.print(";T04=");
+  Serial.print(T04 + random(0, 3), DEC);
+  Serial.print(";T05=");
+  Serial.print(T05 + random(0, 3), DEC);
+  Serial.print(";T06=");
+  Serial.print(T06 + random(0, 3), DEC);
+  Serial.print(";T07=");
+  Serial.print(T07 + random(0, 3), DEC);
+  Serial.print(";T08=");
+  Serial.print(T08 + random(0, 3), DEC);
+  Serial.print(";T09=");
+  Serial.print(T09 + random(0, 3), DEC);
+  Serial.print(";T10=");
+  Serial.print(T10 + random(0, 3), DEC);
+  Serial.print(";T11=");
+  Serial.print(T11 + random(0, 3), DEC);
+  Serial.print(";T12=");
+  Serial.print(T12 + random(0, 3), DEC);
+  Serial.print(";T13=");
+  Serial.print(T13 + random(0, 3), DEC);
+  Serial.print(";T14=");
+  Serial.print(T14 + random(0, 3), DEC);
+  Serial.print(";T15=");
+  Serial.print(T15 + random(0, 3), DEC);
+  Serial.print(";T16=");
+  Serial.print(T16 + random(0, 3), DEC);
+  Serial.print(";T17=");
+  Serial.print(T17 + random(0, 3), DEC);
+  Serial.print(";T18=");
+  Serial.print(T18 + random(0, 3), DEC);
+  Serial.print(";T191=");
+  Serial.print(T19 + random(0, 3), DEC);
+  Serial.print(";T20=");
+  Serial.print(T20 + random(0, 3), DEC);
+  Serial.print(";T21=");
+  Serial.print(T21 + random(0, 3), DEC);
+  Serial.println(";");
+}
+void PrintPres(void)
+{
+
+  // Temporary debug
+  HTStemp = 13; //HTS.readTemperature() + 0.5;
+  int HTStempi = HTStemp;
+  int Pressi = 990; //(BARO.readPressure() + 0.5)*10;
+
+  Serial.print("P00=");
+  Serial.print(Pressi);
+  Serial.print(";P01=");
+  Serial.print(Pressi);
+  Serial.print(";P02=");
+  Serial.print(Pressi);
+  Serial.print(";P03=");
+  Serial.print(Pressi);
+  Serial.print(";P04=");
+  Serial.print(Pressi);
+  Serial.print(";P05=");
+  Serial.print(Pressi);
+  Serial.print(";P06=");
+  Serial.print(Pressi);
+  Serial.print(";P07=");
+  Serial.print(Pressi);
+  Serial.print(";P08=");
+  Serial.print(Pressi);
+  Serial.print(";P09=");
+  Serial.print(Pressi);
+  Serial.println(";");
+}
+
+void PrintData(void)
+{
+
+  // Temporary debug
+  HTStemp = 13; //HTS.readTemperature() + 0.5;
+  int HTStempi = HTStemp;
+  int Pressi = 990; //(BARO.readPressure() + 0.5)*10;
+
+  Serial.print("T00=");
+  Serial.print(T00 + random(0, 3), DEC);
+  Serial.print(";T01=");
+  Serial.print(T01 + random(0, 3), DEC);
+  Serial.print(";T02=");
+  Serial.print(T02 + random(0, 3), DEC);
+  Serial.print(";T03=");
+  Serial.print(T03 + random(0, 3), DEC);
+  Serial.print(";T04=");
+  Serial.print(T04 + random(0, 3), DEC);
+  Serial.print(";T05=");
+  Serial.print(T05 + random(0, 3), DEC);
+  Serial.print(";T06=");
+  Serial.print(T06 + random(0, 3), DEC);
+  Serial.print(";T07=");
+  Serial.print(T07 + random(0, 3), DEC);
+  Serial.print(";T08=");
+  Serial.print(T08 + random(0, 3), DEC);
+  Serial.print(";T09=");
+  Serial.print(T09 + random(0, 3), DEC);
+  Serial.print(";T10=");
+  Serial.print(T10 + random(0, 3), DEC);
+  Serial.print(";T11=");
+  Serial.print(T11 + random(0, 3), DEC);
+  Serial.print(";T12=");
+  Serial.print(T12 + random(0, 3), DEC);
+  Serial.print(";T13=");
+  Serial.print(T13 + random(0, 3), DEC);
+  Serial.print(";T14=");
+  Serial.print(T14 + random(0, 3), DEC);
+  Serial.print(";T15=");
+  Serial.print(T15 + random(0, 3), DEC);
+  Serial.print(";T16=");
+  Serial.print(T16 + random(0, 3), DEC);
+  Serial.print(";T17=");
+  Serial.print(T17 + random(0, 3), DEC);
+  Serial.print(";T18=");
+  Serial.print(T18 + random(0, 3), DEC);
+  Serial.print(";T191=");
+  Serial.print(T19 + random(0, 3), DEC);
+  Serial.print(";T20=");
+  Serial.print(T20 + random(0, 3), DEC);
+  Serial.print(";T21=");
+  Serial.print(T21 + random(0, 3), DEC);
+
+  Serial.print(";P00=");
+  Serial.print(Pressi);
+  Serial.print(";P01=");
+  Serial.print(Pressi);
+  Serial.print(";P02=");
+  Serial.print(Pressi);
+  Serial.print(";P03=");
+  Serial.print(Pressi);
+  Serial.print(";P04=");
+  Serial.print(Pressi);
+  Serial.print(";P05=");
+  Serial.print(Pressi);
+  Serial.print(";P06=");
+  Serial.print(Pressi);
+  Serial.print(";P07=");
+  Serial.print(Pressi);
+  Serial.print(";P08=");
+  Serial.print(Pressi);
+  Serial.print(";P09=");
+  Serial.print(Pressi);
   Serial.println(";");
 }
